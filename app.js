9321391485352563f350a73cf2e59036496ea8c8
Raul Monraz <raulmonraz@gmail.com>
2016-11-01 20:02:30 +0000

    Working on event detail [WIP]

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
 app.get('/report', reportControllers.showReport);
 app.get('/events/:id', eventControllers.eventDetail);
-//app.get(/^\/events\/[0-9]+$/, eventControllers.eventDetail);
+
 
 
 module.exports = app;
